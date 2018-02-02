b7cfc8cf3db7e7a8601bb08eecf3fdf864c357a3
ksasikumar <30828360+ksasikumar@users.noreply.github.com>
2018-02-02 08:53:22 +0000

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the value");
+scanf("%d",&n);
+
+    if(n%2==0)
+{    
+    printf("even");
+}
+
+
+else
+{
+    printf("odd");
+}
+return 0;
+
+}
