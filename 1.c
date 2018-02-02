5bc026a50f7e10f6ccb1b04639298a34cf914d0e
ksasikumar <30828360+ksasikumar@users.noreply.github.com>
2018-02-02 08:45:47 +0000

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int i;
+printf("enter the value");
+scanf("%d",&i);
+{
+    if(i>0)
+{
+    printf("enter the positive");
+}
+
+else if(i<0)
+{
+printf("enter the nagative number");
+
+}
+else
+{
+    printf("ente the value zero");
+}
+return 0;
+}
+}
