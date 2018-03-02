147cc37cae08a6f8bf029ce9281eacb6a6ae4c5f
ksasikumar <30828360+ksasikumar@users.noreply.github.com>
2018-03-02 16:22:39 +0530

    Create 67.c

---
+++ 67.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a,n;
+    scanf("%d",&n);
+    
+    a=n+1;
+    while(a!=0)
+    {
+        if(a%10==0)
+        {
+            printf("%d",a);
+            break;
+        }
+        a++;
+    }
+    
+  
+}
+
