7c6d2ac0716a68760313dc70fd68a0ec92ecf29f
ksasikumar <30828360+ksasikumar@users.noreply.github.com>
2018-03-09 16:10:01 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,16 @@
+#pragma<stdio.h>
+void main()
+{
+    int i,n,c=0;
+    printf ("Enter a number \n");
+    scanf ("%d",&n);
+    for (i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        c=c+1;
+    }
+    if (c==2)
+    printf ("The number is PRIME");
+    else
+    printf ("The number is COMPOSITE");
+}
